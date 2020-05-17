b91b07c9daec487486831c63673078a2d6d1c07a
KinDark <dkinicki@wp.pl>
2020-05-17 12:42:09 +0200

    Moje projekty 2

---
+++ src/main/java/myProjektTarr2019java6Dar/zaawansowana/oop/zadanie1/animals/mammals/Lion.java
@@ -0,0 +1,9 @@
+package myProjektTarr2019java6Dar.zaawansowana.oop.zadanie1.animals.mammals;
+
+public class Lion {
+    public class Lion extends Mammal implements Predator {
+        public Lion(String name) (super(name)); {
+
+        }
+    }
+}
